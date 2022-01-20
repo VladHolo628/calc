3a99ea5344e22fb7a953bb448322b39388c08a6d
Vlad Holo <holovlad@yandex.ru>
2022-01-21 04:18:10 +1000

    re-done w/ switch statement

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ function calc(a, b, operator) {
   let isValidInput =
     typeof a === "number" &&
     typeof b === "number" &&
-    typeof operator !== "undefined";
+    typeof operator !== "undefined" &&
+    !isNaN(a) &&
+    !isNaN(b);
 
   if (isValidInput) {
-    if (operator === "sum") {
-      return a + b;
-    } else if (operator === "minus") {
-      return a - b;
-    } else if (operator === "multy") {
-      return a * b;
-    } else if (operator === "divide") {
-      return a / b;
-    } else {
-      return "Unknown operation";
+    switch (operator) {
+      case "plus":
+        return a + b;
+      case "minus":
+        return a - b;
+      case "multy":
+        return a * b;
+      case "divide":
+        return a / b;
+      default:
+        return "Unknown operation";
     }
   }
   return "Error";
 }
-console.log(calc(20, 5, "divide"));
+console.log(calc(20, 5, "multy"));
