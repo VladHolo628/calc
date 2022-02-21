0818869a3f10c86f42c7f4bbcf6cc8839af51327
Vlad Holo <holovlad@yandex.ru>
2022-02-21 10:59:06 +1000

    Refactored /w modules

--- js/main.js
+++ js/main.js
@@ -23,51 +23,43 @@ function calc(a, b, operator) {
   return "Error";
 }
 
-const buttons = document.querySelectorAll(".calc__button");
-const clearButton = document.querySelector(".calc__button--clear");
-const deleteButton = document.querySelector(".calc__button--delete");
-const equalsButton = document.querySelector(".calc__button--equals");
-const output = document.querySelector(".calc__result");
+import {
+  buttons,
+  clearButton,
+  deleteButton,
+  equalsButton,
+  output,
+} from "./view.js";
+
 const avaliableNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const avaliableOperations = ["+", "-", "÷", "×"];
+const avaliableOperations = {
+  "+": "sum",
+  "-": "sub",
+  "÷": "div",
+  "×": "multi",
+};
+const defaultValue = "0";
 let first = "";
 let second = "";
-let sign = "";
+let sign;
 
-function convertOperator(operator) {
-  switch (operator) {
-    case "+":
-      return "sum";
-
-    case "-":
-      return "sub";
-
-    case "×":
-      return "multi";
-
-    case "÷":
-      return "div";
-
-    default:
-  }
-}
 clearButton.addEventListener("click", () => {
   first = "";
   second = "";
-  output.textContent = "0";
+  output.textContent = defaultValue;
 });
 
 deleteButton.addEventListener("click", () => {
   if (output.textContent.length === 1) {
-    if (sign === "") {
+    if (!sign) {
       first = "";
-      output.textContent = "0";
+      output.textContent = defaultValue;
     } else {
       second = "";
-      output.textContent = "0";
+      output.textContent = defaultValue;
     }
   } else {
-    if (sign === "") {
+    if (!sign) {
       first = first.slice(0, -1);
       output.textContent = first;
     } else {
@@ -77,30 +69,48 @@ deleteButton.addEventListener("click", () => {
   }
 });
 equalsButton.addEventListener("click", () => {
-  if (first !== "" && second !== "" && sign !== "") {
-    output.textContent = calc(+first, +second, convertOperator(sign));
+  if (first && second && sign) {
+    output.textContent = calc(parseInt(first), parseInt(second), sign);
     first = output.textContent;
     second = "";
-    sign = "";
+    sign = null;
   }
 });
+
 for (let btn of buttons) {
   btn.addEventListener("click", (event) => {
     let currentButton = event.target.textContent.trim();
-    if (avaliableNumbers.includes(currentButton) && sign === "") {
+
+    let passBtn = function (btn) {
       output.textContent = "";
-      first += currentButton;
-      output.textContent = first;
-    }
-    if (avaliableOperations.includes(currentButton)) {
-      output.textContent = "";
-      sign += currentButton;
-      output.textContent += currentButton;
-    }
-    if (avaliableNumbers.includes(currentButton) && sign !== "") {
-      output.textContent = "";
-      second += currentButton;
-      output.textContent += second;
+      switch (btn) {
+        case first:
+          first += currentButton;
+          output.textContent = first;
+          break;
+        case second:
+          second += currentButton;
+          output.textContent = second;
+          break;
+        case sign:
+          sign = avaliableOperations[currentButton];
+          output.textContent = currentButton;
+          break;
+
+        default:
+          "Error";
+      }
+    };
+    const isFirstNumber = avaliableNumbers.includes(currentButton) && !sign;
+    const isSign = currentButton in avaliableOperations;
+    const isSecondNumber = avaliableNumbers.includes(currentButton) && sign;
+
+    if (isFirstNumber) {
+      passBtn(first);
+    } else if (isSecondNumber) {
+      passBtn(second);
+    } else if (isSign) {
+      passBtn(sign);
     }
   });
 }
---
+++ js/view.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".calc__button");
+const clearButton = document.querySelector(".calc__button--clear");
+const deleteButton = document.querySelector(".calc__button--delete");
+const equalsButton = document.querySelector(".calc__button--equals");
+const output = document.querySelector(".calc__result");
+
+export { buttons, clearButton, deleteButton, equalsButton, output };
